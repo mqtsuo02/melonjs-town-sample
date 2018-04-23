38e02b7f86c11e2a17341c32fa47cd092bd7bae8
mqtsuo02 <obu.tokyo@gmail.com>
2018-04-24 03:35:53 +0900

    add: tmx & tsx

---
+++ data/map/overworld_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="overworld_tiles" tilewidth="32" tileheight="32" tilecount="80" columns="10">
+ <image source="../img/overworld_tiles.png" width="320" height="256"/>
+</tileset>
